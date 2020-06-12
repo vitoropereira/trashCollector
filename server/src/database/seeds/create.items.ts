c4e683d7c82d924fe26f0eedeb69c9c4c7421643
Vitor <vop1234@hotmail.com>
2020-06-11 23:55:07 -0300

    Finishing the first web part

--- server/src/database/seeds/create.items.ts
+++ server/src/database/seeds/create.items.ts
@@ -6,6 +6,7 @@ export async function seed(knex: Knex) {
     { title: 'Pilhas e Baterias', image: "baterias.svg" },
     { title: 'Ppéis e Papelão', image: "papeis-papelao.svg" },
     { title: 'Resíduos Orgânico', image: "organicos.svg" },
+    { title: 'Eletrônicos', image: "eletronicos.svg" },
     { title: 'Óleo de Cozinha', image: "oleo.svg" },
   ])
 }
